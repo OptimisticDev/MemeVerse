0a01389e1133287e5ff4d53be9904f2b57bc31f1
Moniruzzaman <monir150227@gmail.com>
2021-01-06 10:17:51 +0600

    Development mode

--- backend/app.js
+++ backend/app.js
@@ -17,21 +17,21 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-} else {
-  app.use((req, res, next) => {
-    console.log("It works");
-    next();
-  });
-}
-
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+
+// const __dirname = path.resolve();
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// } else {
+//   app.use((req, res, next) => {
+//     console.log("It works");
+//     next();
+//   });
+// }
 
 export default app;
